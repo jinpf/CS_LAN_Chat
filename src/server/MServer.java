f84cb008f8dfdc208c8d95a12fb052ec25f51950
jinpf <jpflcj@sina.com>
2014-06-28 09:18:42 +0800

    add mserver
    add mserver

---
+++ src/server/MServer.java
@@ -0,0 +1,14 @@
+package server;
+
+import java.net.ServerSocket;
+
+public class MServer {
+	private ServerSocket Mserver;
+	private boolean state; 
+	
+	public MServer(ServerSocket ms) {
+		Mserver=ms;
+		
+	}
+
+}
